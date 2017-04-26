deef4395c08036e5a954be9b421cf84417ecdc37
Sanjeev Reddy <sanjeev96red@gmail.com>
2017-04-26 02:19:30 -0700

    added map icons + infowindows, recentered map location, prepopulated some dining halls on campus

--- starter-node-angular/public/js/controllers/MainCtrl.js
+++ starter-node-angular/public/js/controllers/MainCtrl.js
@@ -1,7 +1,8 @@
 angular.module('MainCtrl', ['gservice']).controller('MainController', function($scope,gservice,$location) {
 
 	$scope.tagline = 'To the moon and back!';	
-	gservice.refresh(37.78,-122.431 );
+        //TODO change this to the user's current location, centered on campus
+	gservice.refresh(32.8800604, -117.2340135);
 	
     /*
 	    function below will take our app to another URL/route ie go('/chatBot') takes us
